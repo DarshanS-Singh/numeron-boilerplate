e399730bf7d7d5e67c7b961d0e1c3c51b2109f39
Darshan Singh <darshan.singhkav@gmail.com>
2023-10-11 14:55:06 +0530

    final code uploAD

--- score.js
+++ score.js
@@ -1,1 +1,12 @@
 // Iteration 5: Store the player score and display it on the game over screen
+let score = localStorage.getItem("score");
+
+var scoreboard = document.getElementById("score-board");
+
+var playAgaininButton = document.getElementById("play-again-button");
+
+scoreboard.innerHTML = score;
+
+playAgaininButton,onclick = () => {
+    location.href = "./game.html";
+};
